7d46136309fb3edcd495914e127c0345a5e7ee9a
Mihaly Dombi <mihaly_dombi@epam.com>
2022-06-09 21:20:06 +0200

    - Added sale_price property to Product. - Added api_view decorator to api_home and modified method.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,9 @@
-from wsgiref import headers
-from django.http import HttpResponse, JsonResponse
 import json
+# from django.http import HttpResponse, JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from products.models import Product
 from django.forms.models import model_to_dict
 
@@ -27,13 +30,13 @@ from django.forms.models import model_to_dict
     data['content_type'] = request.content_type
     return JsonResponse(data) """
 
-
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
+    '''
+    DRF API View
+    '''
     model_data = Product.objects.all().order_by("?").first() # This gives back a random object from the Product table
     data = {}
     if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
-        # data = dict(data)
-        # json_data_str = json.dumps(data)
-    # return HttpResponse(json_data_str, headers={'content-type': 'application/json'})
+        data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
+    return Response(data)
